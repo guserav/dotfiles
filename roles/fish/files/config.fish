ab556922870357fd69e92f4778541b6e4d589832
Daniel Schäfer <ds@struckmeierfliesen.de>
2017-09-23 02:38:48 +0200

    Use vi keybindings in fish

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -2,5 +2,7 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
+fish_vi_key_bindings
+
 xcape -e 'Caps_Lock=Escape'                                      # Map Caps-Lock to Escape
 setxkbmap -option 'caps:ctrl_modifier'                           # Map Caps-Lock-modifier to Ctrl
