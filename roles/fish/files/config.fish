9467b90e5609605821352e8bc77eabe7ee424313
Daniel Schäfer <ds@struckmeierfliesen.de>
2017-09-23 18:19:34 +0200

    Source xcape only when necessary

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -4,5 +4,6 @@ alias ....="cd ../../.."
 
 fish_vi_key_bindings
 
-xcape -e 'Caps_Lock=Escape'                                      # Map Caps-Lock to Escape
-setxkbmap -option 'caps:ctrl_modifier'                           # Map Caps-Lock-modifier to Ctrl
+if test -e xcape.config
+    source xcape.config
+end
