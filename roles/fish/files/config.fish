fef8d6d04debbf2adb1a748c19dd3f3d7e6b53c5
Daniel Schäfer <ds@struckmeierfliesen.de>
2017-09-23 02:37:53 +0200

    Configure caps to ctrl/esc remapping with ansible

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,3 +1,6 @@
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
+
+xcape -e 'Caps_Lock=Escape'                                      # Map Caps-Lock to Escape
+setxkbmap -option 'caps:ctrl_modifier'                           # Map Caps-Lock-modifier to Ctrl
