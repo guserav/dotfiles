1ebb2a8ae7ad74c95cee35f667bd801a8c5c1f4c
Daniel Schäfer <ds@struckmeierfliesen.de>
2017-10-26 11:18:17 +0200

    Install arandr and put screen scripts in playbook

---
+++ roles/multiscreen/files/singleMonitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1366x768 --pos 304x1080 --rotate normal --output DP2 --off
--- screenlayouts/singleMonitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1366x768 --pos 304x1080 --rotate normal --output DP-2 --off
