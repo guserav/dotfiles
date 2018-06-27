fcff87603d093bc03b7d21add690848ce005da34
guserav <erik.zeiske@web.de>
2018-06-27 09:52:02 +0200

    Adjust fish_prompt for WSL

--- roles/fish/files/functions/fish_prompt.fish
+++ roles/fish/files/functions/fish_prompt.fish
@@ -6,6 +6,8 @@ function fish_prompt --description 'Write out the prompt'
     function _is_git_dirty
         echo (git status -s --ignore-submodules=dirty ^/dev/null)
     end
+
+
     if [ (_git_branch_name) ]
         set -l git_branch (set_color -o blue)(_git_branch_name)
         if [ (_is_git_dirty) ]
@@ -36,25 +38,25 @@ function fish_prompt --description 'Write out the prompt'
         set git_info "(git$git_status$git_branch"(set_color black)")"
     end
     printf '%s[%s] %s%s%s%s%s%s%s%s%s'\
-    (set_color black)                  \
+    (set_color normal)                  \
     (date "+%H:%M:%S")              \
     (set_color blue)                  \
     $USER                              \
-    (set_color white)                  \
-    '❙'                                \
+    (set_color normal)                  \
+    '  '                                \
     (set_color green)                 \
     (echo $PWD | sed -e "s|^$HOME|~|") \
     (set_color black)                  \
     $git_info                          \
     (set_color black)
     if test $laststatus -eq 0
-        printf "%s %s%s \f\r≻%s "  \
+        printf "%s %s%s \f\r>%s "  \
         (set_color -o green)\
 	$laststatus         \
         (set_color black)   \
         (set_color normal)
     else
-        printf "%s %s%s \f\r≻%s "  \
+        printf "%s %s%s \f\r>%s "  \
         (set_color -o red)  \
 	$laststatus         \
         (set_color black)   \
