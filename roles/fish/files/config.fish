7751df25ba67c3b487497804d33cb55ba43e97ad
guserav <erik.zeiske@web.de>
2018-12-13 22:06:55 +0100

    Show git status per default

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -5,6 +5,8 @@ alias ....="cd ../../.."
 # Stay in the folger navigated to when exiting ranger
 alias ranger="ranger --choosedir=$HOME/.rangerdir; cd (cat $HOME/.rangerdir)"
 
+# Show git status per default
+set -g _show_git_status "show"
 
 # Allow easy mount of shared folders in vm
 alias mountVM="vmhgfs-fuse /mnt -o umask=22"
