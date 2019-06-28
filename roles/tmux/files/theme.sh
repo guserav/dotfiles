b0535ece79cf262a9855134a3aba945bfeb21f52
guserav <erik.zeiske@web.de>
2019-06-27 22:55:42 -0700

    tmux: Cleanup theme and activity alert

--- roles/tmux/files/theme.sh
+++ roles/tmux/files/theme.sh
@@ -1,53 +1,8 @@
-## source: http://www.hamvocke.com/blog/a-guide-to-customizing-your-tmux-conf/
-## panes
-#set -g pane-border-fg black
-#set -g pane-active-border-fg brightred
-#
-### Status bar design
-## status line
-#set -g status-justify left
-#set -g status-bg default
-set -g status-fg colour12
-#set -g status-interval 2
-#
-## messaging
-#set -g message-fg black
-#set -g message-bg yellow
-#set -g message-command-fg blue
-#set -g message-command-bg black
-#
-##window mode
-#setw -g mode-bg colour6
-#setw -g mode-fg colour0
-#
-## window status
-#setw -g window-status-format " #F#I:#W#F "
-#setw -g window-status-current-format " #F#I:#W#F "
-#setw -g window-status-format "#[fg=magenta]#[bg=black] #I #[bg=cyan]#[fg=colour8] #W "
-#setw -g window-status-current-format "#[bg=brightmagenta]#[fg=colour8] #I #[fg=colour8]#[bg=colour14] #W "
-#setw -g window-status-current-bg colour0
-#setw -g window-status-current-fg colour11
-#setw -g window-status-current-attr dim
-#setw -g window-status-bg green
-#setw -g window-status-fg black
-#setw -g window-status-attr reverse
-#
-#
-## The modes {{{
-#setw -g clock-mode-colour colour135
-#setw -g mode-attr bold
-#setw -g mode-fg colour196
-#setw -g mode-bg colour238
-#
-## }}}
-#
 ## The panes {{{
-#
 set -g pane-border-bg colour235
-set -g pane-border-fg colour238
+set -g pane-border-fg colour235
 set -g pane-active-border-bg colour236
 set -g pane-active-border-fg colour51
-#
 ## }}}
 #
 ## The statusbar {{{
@@ -56,21 +11,22 @@ set -g status-bg colour234
 set -g status-fg colour137
 #set -g status-attr dim
 set -g status-right ' #[fg=colour137,bg=colour237,bold] #h #[fg=colour233,bg=colour241,bold] %d/%m #[fg=colour233,bg=colour245,bold] %H:%M:%S '
-#
+## general window tittle{{{
+setw -g window-status-fg colour138
+setw -g window-status-bg colour235
+setw -g window-status-attr none
+setw -g window-status-format ' #I#[fg=colour237]:#[fg=colour250]#W#{?window_activity_flag,#[fg=colour1],#[fg=colour244]}#F '
+## }}}
+## current window title{{{
 setw -g window-status-current-fg colour81
 setw -g window-status-current-bg colour238
 setw -g window-status-current-attr bold
 setw -g window-status-current-format ' #I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#{?window_zoomed_flag,#[fg=colour40]Z , }' # no asterisk #F
-#
-setw -g window-status-fg colour138
-setw -g window-status-bg colour235
-setw -g window-status-attr none
-setw -g window-status-format ' #I#[fg=colour237]:#[fg=colour250]#W#[fg=colour244]#F '
-#
-#setw -g window-status-bell-attr bold
-#setw -g window-status-bell-fg colour233
-#setw -g window-status-bell-bg colour125
-#
+## }}}
+## bell or activity {{{
+setw -g window-status-bell-style bold,fg=colour138,bg=colour235
+setw -g window-status-activity-style bold,fg=colour138,bg=colour235
+# }}}
 ## }}}
 #
 ## The messages {{{
