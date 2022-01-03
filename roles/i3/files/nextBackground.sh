f4137f3e4f44addff0f17483332fef31827e8fef
guserav <erik.zeiske@web.de>
2022-01-03 23:28:08 +0100

    Expand script to work for 3 backgrounds

--- roles/i3/files/nextBackground.sh
+++ roles/i3/files/nextBackground.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 [[ \$- == *i*   ]] || set -eu -o pipefail
-IMAGE=`find ~/Bilder/Hintergrund/ -name \* -type f | shuf | head -n 2`
+IMAGE=`find ~/Bilder/Hintergrund/ -name \* -type f | shuf | head -n 3`
 feh --bg-max $IMAGE
